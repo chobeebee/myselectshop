f4653cf68b09574d7969a10d093908a56d44cc64
chobeebee <websera12@gmail.com>
2025-02-05 16:08:22 +0900

    SQL 작성해보기

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -11,5 +11,10 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
+    //select * from product p where p.user_id= 1;
+    //select p.id, p.title as product_title, pf.product_id as product_id, pf.folder_id as folder_id
+    //from product p left join product_folder pf on p.id = pf.product_id where p.user_id = 1 and pf.folder_id = 3
+    // order by p.id desc
+    // limit 0, 10; //개수 제한 : limit(시작 숫자, 개수)
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
